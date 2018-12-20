9ad1f1eefe588eb8c4b27de7279aae43b051bb18
876584635678890 <43884492+876584635678890@users.noreply.github.com>
2018-12-20 11:25:58 +0000

    Formatting

--- main.js
+++ main.js
@@ -1,40 +1,14 @@
 const chartConfig = {
   type: 'line',
-  data: {
-    labels: [],
-    datasets: [
-      { data: [], borderColor: '#a3a3d3', backgroundColor: '#a2bbd342' }
-    ]
-  },
+  data: { labels: [], datasets: [{ data: [], borderColor: '#a3a3d3', backgroundColor: '#a2bbd342' }] },
   options: {
-    title: {
-      display: true,
-      text: ''
-    },
-    hover: {
-      mode: 'nearest',
-      intersect: false
-    },
-    tooltips: {
-      mode: 'nearest',
-      intersect: false,
-      backgroundColor: 'hsla(240,25%,76%,.81)',
-      displayColors: false
-    },
-    legend: {
-      display: false
-    },
+    title: { display: true, text: '' },
+    hover: { mode: 'nearest', intersect: false },
+    tooltips: { mode: 'nearest', intersect: false, backgroundColor: 'hsla(240,25%,76%,.81)', displayColors: false },
+    legend: { display: false },
     scales: {
-      yAxes: [
-        { display: true, scaleLabel: { display: true, labelString: 'Elo' } }
-      ],
-      xAxes: [
-        {
-          display: true,
-          scaleLabel: { display: true, labelString: 'Opponents' },
-          ticks: { display: false }
-        }
-      ]
+      yAxes: [{ display: true, scaleLabel: { display: true, labelString: 'Elo' } }],
+      xAxes: [{ display: true, scaleLabel: { display: true, labelString: 'Opponents' }, ticks: { display: false } }]
     }
   }
 }
@@ -42,42 +16,30 @@ let chart = undefined
 const K = 32
 
 async function plot() {
-  if (window.location.search == '') {
-    document.getElementById('loading').innerHTML = ''
-    return
-  } else document.getElementById('loading').innerText = 'loading..'
-  const algoId = window.location.search.split('?')[1].split('id=')[1]
+  if (window.location.search == '') return document.getElementById('loading').innerHTML = ''
+  else if (window.location.search.length < 5) return document.getElementById('loading').innerText = 'empty id'
+  else document.getElementById('loading').innerText = 'loading..'
+  // fetch response
+  const algoId = window.location.search.split('?id=')[1]
   document.getElementById('algo-id').value = algoId
-  const fetched = await fetch(
-    `https://terminal.c1games.com/api/game/algo/${algoId}/matches`
-  )
-  if (fetched.status != 200) {
-    document.getElementById('loading').innerText = 'failed to retrieve data'
-    return
-  }
+  const fetched = await fetch(`https://terminal.c1games.com/api/game/algo/${algoId}/matches`)
+  if (fetched.status != 200) return document.getElementById('loading').innerText = 'failed to retrieve data'
   const response = await fetched.json()
-  const matches = response.data.matches
+  const matches = response.data.matches.reverse()
   if (!(matches.length > 0)) return
-  const algoData =
-    matches[0].winning_algo.id == algoId
-      ? matches[0].winning_algo
-      : matches[0].losing_algo
-  const { name, elo } = algoData
-  matches.reverse()
-
+  const { name, elo } = matches[0].winning_algo.id == algoId ? matches[0].winning_algo : matches[0].losing_algo
+  // update config
   document.getElementById('loading').innerHTML = ''
   document.title = `${name} - matches`
-
   chartConfig.options.title.text = `"${name}" elo over matches`
   chartConfig.data.datasets[0].data.length = 0
   chartConfig.data.labels.length = 0
   let previousElo = 1500
   const frequency = new Map()
-
-  const fullTable = document.getElementById('fullTable')
+  // full table
+  const fullTable = document.getElementById('full-table')
   let previousRows = fullTable.rows.length
   for (i = 0; i < previousRows; i++) fullTable.deleteRow(0)
-
   let wins = 0
   matches.forEach((match, index) => {
     const won = match.winning_algo.id == algoId
@@ -86,64 +48,34 @@ async function plot() {
     if (won) wins++
     frequency.set(opponent.user, (frequency.get(opponent.user) || 0) + 1)
     chartConfig.data.labels.push(opponent.name)
-    chartConfig.data.datasets[0].data.push(
-      index == matches.length - 1
-        ? elo
-        : Math.round(
-            (previousElo +=
-              K *
-              ((won ? 1 : 0) -
-                1 / (1 + Math.pow(10, (opponent.elo - previousElo) / 400))))
-          )
-    )
+    chartConfig.data.datasets[0].data.push(index == matches.length - 1 ? elo : Math.round((previousElo += K * ((won ? 1 : 0) - 1 / (1 + Math.pow(10, (opponent.elo - previousElo) / 400))))))
   })
-  document.getElementById('fullTitle').innerHTML = `All games`
-  document.getElementById(
-    'fullStats'
-  ).innerHTML = `Wins: ${wins} Losses: ${matches.length - wins}`
+  document.getElementById('full-title').innerHTML = `All games`
+  document.getElementById('full-stats').innerHTML = `Wins: ${wins} Losses: ${matches.length - wins}`
   tableTop(fullTable)
-
-  const rivalTable = document.getElementById('rivalTable')
+  // rival table
+  const rivalTable = document.getElementById('rival-table')
   previousRows = rivalTable.rows.length
   for (i = 0; i < previousRows; i++) rivalTable.deleteRow(0)
-
   const frequencyValues = Array.from(frequency.values())
   const maximumFrequency = Math.max(...frequencyValues)
-  const rival = Array.from(frequency.keys())[
-    frequencyValues.findIndex(value => value == maximumFrequency)
-  ]
-
-  document.getElementById('rivalTitle').innerHTML = `Rival: ${rival}`
-  document.getElementById(
-    'rivalStats'
-  ).innerHTML = `Games played: ${maximumFrequency}`
+  const rival = Array.from(frequency.keys())[frequencyValues.findIndex(value => value == maximumFrequency)]
+  document.getElementById('rival-title').innerHTML = `Rival: ${rival}`
+  document.getElementById('rival-stats').innerHTML = `Games played: ${maximumFrequency}`
   matches.forEach(match => {
-    if (match.winning_algo.user != rival && match.losing_algo.user != rival)
-      return
+    if (match.winning_algo.user != rival && match.losing_algo.user != rival) return
     insertMatch(rivalTable, match, algoId)
   })
   tableTop(rivalTable)
 
-  if (chart == undefined)
-    chart = new Chart(
-      document.getElementById('chart').getContext('2d'),
-      chartConfig
-    )
+  if (chart == undefined) chart = new Chart(document.getElementById('chart').getContext('2d'), chartConfig)
   else chart.update()
 }
 
 function insertMatch(table, match, algoId) {
   const won = match.winning_algo.id == algoId
   const opponent = won ? match.losing_algo : match.winning_algo
-  insertEntry(table, [
-    opponent.name,
-    won ? 'W' : 'L',
-    match.turns,
-    opponent.elo,
-    `<a href='https://terminal.c1games.com/watch/${
-      match.id
-    }' target='_blank'>watch</a>`
-  ])
+  insertEntry(table, [opponent.name, won ? 'W' : 'L', match.turns, opponent.elo, `<a href='https://terminal.c1games.com/watch/${match.id}' target='_blank'>watch</a>`])
 }
 
 function tableTop(table) {
@@ -156,8 +88,7 @@ function insertEntry(table, cells) {
 }
 
 window.onload = () => {
-  document.getElementById('find').onclick = () =>
-    (window.location.search = `?id=${document.getElementById('algo-id').value}`)
+  document.getElementById('find').onclick = () => window.location.search = `?id=${document.getElementById('algo-id').value}`
   document.getElementById('update').onclick = () => plot()
   plot()
 }
